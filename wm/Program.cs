a85039e51affa4ac4492eac1949c5065b6c13a1d
Kevin Weidner <weidnerk@gmail.com>
2019-11-23 17:52:08 -0500

    Use new SellerListing property on Listing.

--- wm/Program.cs
+++ wm/Program.cs
@@ -10,6 +10,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace wm
 {
@@ -46,109 +47,117 @@ namespace wm
             string body = null;
             string oosBody = null;
 
-            var settings = new UserSettingsView();
-            if (storeID == 1)
+            try
             {
-                settings = db.GetUserSettings(connStr, HOME_DECOR_USER_ID);
-            }
-            if (storeID == 4)
-            {
-                settings = db.GetUserSettings(connStr, EAGLE_USER_ID);
-            }
-            var walListings = db.Listings.Where(x => x.SourceID == 1 && !x.OOS && x.Listed != null && x.StoreID == storeID).ToList();
-
-            foreach (Listing listing in walListings)
-            {
-                var wmItem = await wallib.Class1.GetDetail(listing.SourceUrl);
-                Console.WriteLine((++i) + " " + listing.Title);
-                if (wmItem == null)  // could not fetch from walmart website
+                var settings = new UserSettingsView();
+                if (storeID == 1)
                 {
-                    //response = scrapeAPI.ebayAPIs.ReviseItem(settings, listing.ListedItemID, qty: 0);
-                    //await db.UpdateOOS(listing.ListedItemID, true);
+                    settings = db.GetUserSettings(connStr, HOME_DECOR_USER_ID);
+                }
+                if (storeID == 4)
+                {
+                    settings = db.GetUserSettings(connStr, EAGLE_USER_ID);
+                }
+                var walListings = db.Listings.Include(c => c.SellerListing).Where(x => x.SourceID == 1 && !x.OOS && x.Listed != null && x.StoreID == storeID).ToList();
 
-                    Console.WriteLine(listing.Title);
-                    ++outofstock;
-                    string ret = dsutil.DSUtil.SendMailDev("ventures2019@gmail.com", "INVALID URL " + listing.Title, "revise listing");
+                foreach (Listing listing in walListings)
+                {
+                    var wmItem = await wallib.Class1.GetDetail(listing.SourceUrl);
+                    Console.WriteLine((++i) + " " + listing.SellerListing.Title);
+                    if (wmItem == null)  // could not fetch from walmart website
+                    {
+                        //response = scrapeAPI.ebayAPIs.ReviseItem(settings, listing.ListedItemID, qty: 0);
+                        //await db.UpdateOOS(listing.ListedItemID, true);
+
+                        Console.WriteLine(listing.SellerListing.Title);
+                        ++outofstock;
+                        string ret = dsutil.DSUtil.SendMailDev("ventures2019@gmail.com", "INVALID URL " + listing.SellerListing.Title, "revise listing");
+                    }
+                    else
+                    {
+                        if (wmItem.OutOfStock)
+                        {
+                            response = Utility.eBayItem.ReviseItem(settings, listing.ListedItemID, qty: 0);
+                            await db.UpdateOOS(listing.ListedItemID, true);
+                            ++outofstock;
+
+                            oosBody += "<br/><br/>" + listing.SellerListing.Title;
+
+                            //string ret = await dsutil.DSUtil.SendMailProd("ventures2019@gmail.com", "OUT OF STO " + listing.Title, "revise listing", "localhost");
+                            //string ret = dsutil.DSUtil.SendMailDev("ventures2019@gmail.com", "OUT OF STO " + listing.Title, "revise listing");
+                        }
+                        if (wmItem.Price != listing.SupplierPrice)
+                        {
+                            decimal newPrice = Utility.eBayItem.wmNewPrice(wmItem.Price);
+                            response = Utility.eBayItem.ReviseItem(settings, listing.ListedItemID, price: (double)newPrice);
+                            await db.UpdatePrice(listing, (decimal)newPrice, wmItem.Price);
+
+                            ++mispriceings;
+                            body += "<br/><br/>" + "<b>" + listing.SellerListing.Title + "</b>";
+                            body += "<br/><br/>" + listing.ListedItemID + " db supplier price " + listing.SupplierPrice.ToString("c") + " different from just captured " + wmItem.Price.ToString("c");
+
+                            if (wmItem.Price < listing.SupplierPrice)
+                            {
+                                body += "<br/>Supplier dropped their price.";
+                            }
+                            else
+                            {
+                                body += "<br/>Supplier INCREASED their price!";
+                            }
+                            dsutil.DSUtil.WriteFile(_logfile, body, log_username);
+                            body += "<br/><br/>";
+                        }
+                        if (wmItem.ShippingNotAvailable)
+                        {
+                            //response = scrapeAPI.ebayAPIs.ReviseItem(listing.ListedItemID, qty: 0);
+                            //await db.UpdateOOS(listing.ListedItemID, true);
+                            Console.WriteLine(listing.SellerListing.Title);
+                            Console.WriteLine("ShippingNotAvailable");
+                            ++shippingNotAvailable;
+                            //string ret = await dsutil.DSUtil.SendMailProd("ventures2019@gmail.com", "OUT OF STO " + listing.Title, "revise listing", "localhost");
+                            //string ret = dsutil.DSUtil.SendMailDev("ventures2019@gmail.com", "Shipping Not Available " + listing.Title, "revise listing");
+                        }
+                    }
+                }
+                if (mispriceings > 0)
+                {
+                    string title = "PRICE CHANGE ";
+                    if (outofstock > 0)
+                    {
+                        oosBody = "<br/><br/>" + "<b>OUT OF STOCK</b>" + "<br/>" + oosBody;
+                        body += oosBody;
+                        title += "/OUT OF STOCK";
+                    }
+                    string ret = dsutil.DSUtil.SendMailDev("ventures2019@gmail.com", title, body);
                 }
                 else
                 {
-                    if (wmItem.OutOfStock)
+                    if (outofstock > 0)
                     {
-                        response = Utility.eBayItem.ReviseItem(settings, listing.ListedItemID, qty: 0);
-                        await db.UpdateOOS(listing.ListedItemID, true);
-                        ++outofstock;
-
-                        oosBody += "<br/><br/>" + listing.Title;
-
-                        //string ret = await dsutil.DSUtil.SendMailProd("ventures2019@gmail.com", "OUT OF STO " + listing.Title, "revise listing", "localhost");
-                        //string ret = dsutil.DSUtil.SendMailDev("ventures2019@gmail.com", "OUT OF STO " + listing.Title, "revise listing");
-                    }
-                    if (wmItem.Price != listing.SupplierPrice)
-                    {
-                        decimal newPrice = Utility.eBayItem.wmNewPrice(wmItem.Price);
-                        response = Utility.eBayItem.ReviseItem(settings, listing.ListedItemID, price: (double)newPrice);
-                        await db.UpdatePrice(listing, (decimal)newPrice, wmItem.Price);
-
-                        ++mispriceings;
-                        body += "<br/><br/>" + "<b>" + listing.Title + "</b>";
-                        body += "<br/><br/>" + listing.ListedItemID + " db supplier price " + listing.SupplierPrice.ToString("c") + " different from just captured " + wmItem.Price.ToString("c");
-                        
-                        if (wmItem.Price < listing.SupplierPrice)
-                        {
-                            body += "<br/>Supplier dropped their price.";
-                        }
-                        else
-                        {
-                            body += "<br/>Supplier INCREASED their price!";
-                        }
-                        dsutil.DSUtil.WriteFile(_logfile, body, log_username);
-                        body += "<br/><br/>";
-                    }
-                    if (wmItem.ShippingNotAvailable)
-                    {
-                        //response = scrapeAPI.ebayAPIs.ReviseItem(listing.ListedItemID, qty: 0);
-                        //await db.UpdateOOS(listing.ListedItemID, true);
-                        Console.WriteLine(listing.Title);
-                        Console.WriteLine("ShippingNotAvailable");
-                        ++shippingNotAvailable;
-                        //string ret = await dsutil.DSUtil.SendMailProd("ventures2019@gmail.com", "OUT OF STO " + listing.Title, "revise listing", "localhost");
-                        //string ret = dsutil.DSUtil.SendMailDev("ventures2019@gmail.com", "Shipping Not Available " + listing.Title, "revise listing");
+                        oosBody += "<br/><br/>" + "OUT OF STOCK" + "<br/><br/> " + oosBody;
+                        string ret = dsutil.DSUtil.SendMailDev("ventures2019@gmail.com", "OUT OF STOCK ", oosBody);
                     }
                 }
-            }
-            if (mispriceings > 0)
-            {
-                string title = "PRICE CHANGE ";
-                if (outofstock > 0)
+                if (mispriceings == 0 && outofstock == 0)
                 {
-                    oosBody = "<br/><br/>" + "<b>OUT OF STOCK</b>" + "<br/>" + oosBody;
-                    body += oosBody;
-                    title += "/OUT OF STOCK";
+                    string ret = dsutil.DSUtil.SendMailDev("ventures2019@gmail.com", "WM TRACKER", "No discrepencies found.");
                 }
-                string ret = dsutil.DSUtil.SendMailDev("ventures2019@gmail.com", title, body);
+                string msg = "Found " + outofstock.ToString() + " out of stock";
+                dsutil.DSUtil.WriteFile(_logfile, msg, log_username);
+
+                msg = "Found " + mispriceings.ToString() + " mispricings";
+                dsutil.DSUtil.WriteFile(_logfile, msg, log_username);
+
+                msg = "Found " + shippingNotAvailable.ToString() + " shippingNotAvailable";
+                dsutil.DSUtil.WriteFile(_logfile, msg, log_username);
+
+                return outofstock;
             }
-            else
+            catch(Exception exc)
             {
-                if (outofstock > 0)
-                {
-                    oosBody += "<br/><br/>" + "OUT OF STOCK" + "<br/><br/> " + oosBody;
-                    string ret = dsutil.DSUtil.SendMailDev("ventures2019@gmail.com", "OUT OF STOCK ", oosBody);
-                }
+                string msg = exc.Message;
+                return outofstock;
             }
-            if (mispriceings == 0 && outofstock == 0)
-            {
-                string ret = dsutil.DSUtil.SendMailDev("ventures2019@gmail.com", "WM TRACKER", "No discrepencies found.");
-            }
-            string msg = "Found " + outofstock.ToString() + " out of stock";
-            dsutil.DSUtil.WriteFile(_logfile, msg, log_username);
-
-            msg = "Found " + mispriceings.ToString() + " mispricings";
-            dsutil.DSUtil.WriteFile(_logfile, msg, log_username);
-
-            msg = "Found " + shippingNotAvailable.ToString() + " shippingNotAvailable";
-            dsutil.DSUtil.WriteFile(_logfile, msg, log_username);
-
-            return outofstock;
         }
     }
 }
